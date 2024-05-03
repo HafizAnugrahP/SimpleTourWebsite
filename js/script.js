4a958e15676c348cf398b939c918784d64c8c54f
HafizAnugrahP <hafizanugrahpratama@gmail.com>
2024-05-04 06:46:24 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ navMenu.onclick = function () {
 window.addEventListener("scroll", function () {
     header.classList.toggle("down", window.scrollY > 60)
 })
+
+// ============= SCROLL ANIMATION ================= //
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('anim')
+        } else {
+            entry.target.classList.remove('anim')
+        }
+    })
+})
+
+const section = document.querySelectorAll('section')
+section.forEach((el) => observer.observe(el))
